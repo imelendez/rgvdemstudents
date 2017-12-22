30b03c31d255824f3cf820a36233f27d779ac503
Ismael <ismael.melendez@hotmail.com>
2017-12-21 21:59:01 -0600

    Trying out jekyll now

--- fun.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-  document.getElementById("ActionForm").style.display = 'none';
-};
-
-var theButton = document.getElementById('actionbttn');
-theButton.onclick = function() {
-document.getElementById("ActionForm").style.display='inline';
-document.getElementById("ActionForm").style.text-center;
-}
